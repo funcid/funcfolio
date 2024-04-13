ea07a03078093565c204f9133380d7c4208d44df
Artem Tsaryuk <42806772+funcid@users.noreply.github.com>
2024-04-13 22:39:07 +0300

    add new links

--- src/components/SocialMedia.svelte
+++ src/components/SocialMedia.svelte
@@ -6,6 +6,6 @@
     export let link: string;
 </script>
 
-<button on:click={() => window.open(link)} class="text-[32rem] text-black w-[174rem] h-[83rem] flex items-center justify-center gap-[50rem] rounded-[7rem]" style="background-color: var({ color })">
+<button on:click={() => window.open(link)} class="text-[32rem] text-black h-[83rem] flex items-center justify-center gap-[50rem] px-[30rem] rounded-[7rem]" style="background-color: var({ color })">
     {media} {@html LinkIcon} 
 </button>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,8 +10,8 @@
     <div class="flex gap-[80rem]">
         <div class="bg-avatar w-[820rem] h-[514rem] bg-center bg-cover rounded-[7rem] relative overflow-hidden">
 			<div class="absolute bottom-0 left-0 backdrop-blur-[100rem] bg-black/10 w-full h-[234rem]">
-				<div class="p-[30rem] text-[56rem] text-white font-bold">
-					ЦАРЮК АРТЁМ
+				<div class="p-[30rem] text-[56rem] text-white font-bold font-oddval">
+					АРТЁМ ЦАРЮК
 				</div>
 				<div class="px-[30rem] text-[32rem] text-white">
 					Java/Kotlin разработчик
@@ -31,8 +31,10 @@
 			<div class="w-[656rem] text-[32rem]/[150%]">
 				Обладаю обширным опытом в создании Java-сервисов и обработке данных с целью аналитического использования.
 			</div>
-			<div class="w-[656rem] text-[32rem] text-black">
+			<div class="text-[32rem] text-black flex gap-[20rem]">
 				<SocialMedia media="VK" link="https://vk.com/funcid" color="--blue"/>
+				<SocialMedia media="TELEGRAM" link="https://t.me/funcid" color="--skyblue"/>
+				<SocialMedia media="GITHUB" link="https://github.com/funcid" color="--green"/>
 			</div>
         </div>
     </div>
