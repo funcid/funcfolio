2c17b5dbd29de98fd01f949c6431cd4071f94788
Artem Tsaryuk <42806772+funcid@users.noreply.github.com>
2024-04-14 02:20:35 +0300

    add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,12 @@
     import Introduction from '../components/Introduction.svelte';
 	import Experience from '../components/Experience.svelte';
     import Portfolio from '../components/Portfolio.svelte';
+    import Footer from '../components/Footer.svelte';
 </script>
 
 <div class="p-[100rem] text-[32rem] flex flex-col gap-[200rem]">
 	<Introduction/>
 	<Experience/>
 	<Portfolio/>
+	<Footer/>
 </div>
