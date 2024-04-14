3f16587becc2db2dff9a254c57e5b54ac67f0c79
Artem Tsaryuk <42806772+funcid@users.noreply.github.com>
2024-04-14 12:10:03 +0300

    fill project text

--- src/components/Portfolio.svelte
+++ src/components/Portfolio.svelte
@@ -1,6 +1,8 @@
 <script>
-    import DotsIcon from '$lib/images/icon/black_dots.svg?raw'
-    import VisualDriverImage from '$lib/images/project/visual_driver.jpg'
+    import DotsIcon from "$lib/images/icon/black_dots.svg?raw";
+    import SquareIcon from '$lib/images/icon/black_square.svg?raw'
+    import BracketsIcon from '$lib/images/icon/black_brackets.svg?raw'
+    import VisualDriverImage from "$lib/images/project/visual_driver.jpg";
 </script>
 
 <div class="flex flex-col gap-[100rem]">
@@ -8,18 +10,35 @@
         <p class="min-w-[900rem] text-[96rem] font-bold font-oddval">
             ПОРТФОЛИО
         </p>
-        <p class="text-[32rem] font-sans w-full">
-            Пет проекты и опенсорс
-        </p>
+        <p class="text-[32rem] font-sans w-full">Пет проекты и опенсорс</p>
         <div>
             {@html DotsIcon}
         </div>
     </div>
-    <div class="max-w-[1720rem] flex flex-wrap gap-[80rem]">
-        <div class="w-[820rem] h-[519rem] rounded-[7rem] overflow-hidden relative">
-            <img class="w-full object-cover object-top scale-150" src={VisualDriverImage} alt="Visual Driver"/>
-            <div class="absolute bottom-0 w-full h-[314rem] bg-[--blue]">
 
+    <div class="max-w-[1720rem] flex flex-wrap gap-[80rem]">
+        <div class="w-[820rem] h-[519rem] rounded-[7rem] overflow-hidden">
+            <img
+                src={VisualDriverImage}
+                class="h-[calc(100%-314rem)] w-full object-cover object-center"
+                alt="{VisualDriverImage} logo"
+            />
+
+            <div class="w-full h-[314rem] bg-[--blue]">
+                <div class="p-[30rem] h-full flex justify-between gap-[20rem]">
+                    <div class="w-[718rem] flex flex-col justify-between">
+                        <p class="text-[72rem] font-bold font-oddval">
+                            VISUAL<br />DRIVER
+                        </p>
+                        <p class="text-[32rem] font-sens">
+                            Движок для Minecraft, общий набор визуальных решений
+                        </p>
+                    </div>
+                    <div class="w-[22rem] h-full flex flex-col justify-between">
+                        {@html SquareIcon}
+                        {@html BracketsIcon}
+                    </div>
+                </div>
             </div>
         </div>
     </div>
