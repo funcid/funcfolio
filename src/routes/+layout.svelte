ecbf38052385fb2e6d4ea56273eb36c529c9890b
Racionalistas <m.racionalus@gmail.com>
2024-04-14 15:18:09 +0300

    fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,16 @@
 <script>
-    import '../app.css';
-    import Introduction from '../components/Introduction.svelte';
-	import Experience from '../components/Experience.svelte';
-    import Portfolio from '../components/Portfolio.svelte';
-    import Footer from '../components/Footer.svelte';
+	import "../app.css";
+	import Introduction from "../components/Introduction.svelte";
+	import Experience from "../components/Experience.svelte";
+	import Portfolio from "../components/Portfolio.svelte";
+	import Footer from "../components/Footer.svelte";
 </script>
 
-<div class="p-[100rem] text-[32rem] flex flex-col gap-[200rem]">
-	<Introduction/>
-	<Experience/>
-	<Portfolio/>
-	<Footer/>
+<div class="w-full flex justify-center p-[100rem]">
+	<div class="w-full max-w-[1720rem] text-[32rem] flex flex-col gap-[200rem]">
+		<Introduction />
+		<Experience />
+		<Portfolio />
+		<Footer />
+	</div>
 </div>
